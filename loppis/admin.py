7fb3618134b76c2493804c12db943e2a2b7e3f91
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-05-26 15:12:13 +0000

    Created Loppis app, models and county list fixtures

---
+++ loppis/admin.py
@@ -0,0 +1,7 @@
+""" Loppis Admin """
+from django.contrib import admin
+from .models import Loppis, County
+
+# Register your models here.
+admin.site.register(Loppis)
+admin.site.register(County)
---
+++ loppis/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoppisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'loppis'
